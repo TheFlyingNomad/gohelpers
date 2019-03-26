fe97a19117ae198f311a348ac173554506d8847a
codemodify <codemodify@codemodify.io>
2019-03-26 00:13:14 -0700

    save

--- time/time.go
+++ time/time.go
@@ -16,3 +16,14 @@ func ToMicroSeconds(t time.Time) int64 {
 func ToMilliSeconds(t time.Time) int64 {
 	return t.UTC().UnixNano() / int64(time.Millisecond)
 }
+
+// StartOfMonth -
+func StartOfMonth(t time.Time) time.Time {
+	year, month, _ := t.Date()
+	return time.Date(year, month, 1, 0, 0, 0, 0, t.Location())
+}
+
+// EndOfMonth -
+func EndOfMonth(t time.Time) time.Time {
+	return StartOfMonth(t).AddDate(0, 1, -1)
+}
